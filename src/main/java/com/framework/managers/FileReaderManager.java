9ff40c6ac2df280fef217c6dc8b0da1ad2eca23c
jkumud <jkumud@LBNL0205142>
2019-01-02 06:47:50 +0100

    added

--- src/main/java/com/framework/managers/FileReaderManager.java
+++ src/main/java/com/framework/managers/FileReaderManager.java
@@ -4,15 +4,14 @@ import com.framework.dataProvider.ConfigFileReader;
 
 public class FileReaderManager {
 
-    private static FileReaderManager fileReaderManager = new FileReaderManager();
+    private static FileReaderManager fileReaderManager;
     private static ConfigFileReader configFileReader;
 
     private FileReaderManager(){
-
     }
 
     public static FileReaderManager getInstance(){
-        return fileReaderManager;
+        return fileReaderManager == null ?  fileReaderManager = new FileReaderManager() : fileReaderManager;
     }
 
     public ConfigFileReader getConfigFileReader(){
---
+++ src/main/java/com/framework/managers/FileReaderManagerUpdated.java
@@ -0,0 +1,9 @@
+package com.framework.managers;
+
+public enum FileReaderManagerUpdated {
+    INSTANCE;
+
+    public void Show(){
+        System.out.println("Test");
+    }
+}
