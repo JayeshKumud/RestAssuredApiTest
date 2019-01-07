4933a24554e63acb938c4bf953a86779e5a65730
jayeshkumud <jayesh.kumud@gmail.com>
2019-01-07 20:13:59 +0100

    added INSTANCE

---
+++ src/main/java/testNG/RestAssuredConfiguration.java
@@ -0,0 +1,29 @@
+package testNG;
+
+import managers.FileReaderManager;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+
+public class RestAssuredConfiguration {
+
+    @BeforeSuite(alwaysRun = true)
+    public void configure() {
+        RestAssured.baseURI = FileReaderManager.INSTANCE .getConfigFileReader().getBaseURI();
+        RestAssured.basePath = FileReaderManager.INSTANCE.getConfigFileReader().getBasePath();
+    }
+
+    public RequestSpecification getRequestSpecification() {
+        return RestAssured.given().contentType(ContentType.JSON);
+    }
+
+    public Response getResponse(RequestSpecification requestSpecification,String endpoint, int status){
+        Response response = requestSpecification.get(endpoint);
+        Assert.assertEquals(response.getStatusCode(),status);
+        response.then().log().all();
+        return response;
+    }
+}
